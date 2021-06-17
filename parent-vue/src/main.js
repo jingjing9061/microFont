8d11ba10ad18495291a0c35a25c228e4086e46b5
June <1179012421@qq.com>
2021-06-17 11:08:08 +0800

    js 代理沙箱 及样式隔离

--- parent-vue/src/main.js
+++ parent-vue/src/main.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import  {registerApplication,start} from 'single-spa'
+import { registerApplication, start } from 'single-spa'
 
 Vue.config.productionTip = false
 
-async function loadScript(url){
-  return new Promise((resolve,reject)=>{
+async function loadScript(url) {
+  return new Promise((resolve, reject) => {
     let script = document.createElement('script');
     script.src = url;
     script.onload = resolve;
@@ -20,15 +20,15 @@ async function loadScript(url){
 
 
 //myVueApp自定义名字
-registerApplication('myVueApp',async()=>{
-    console.log('加载模块')
-    //systemJS 动态创建一个script 把chunk-vendors文件引入进来
-    await loadScript('http://localhost:10001/js/chunk-vendors.js')
-    await loadScript('http://localhost:10001/js/app.js')
-    return window.singleVue; // bootstrap mount unmount
+registerApplication('myVueApp', async () => {
+  console.log('加载模块')
+  //systemJS 动态创建一个script 把chunk-vendors文件引入进来
+  await loadScript('http://localhost:10001/js/chunk-vendors.js')
+  await loadScript('http://localhost:10001/js/app.js')
+  return window.singleVue; // bootstrap mount unmount
 
 },
- location =>location.pathname.startsWith('/vue')//用户切换到/vue的路径下，我们需要加载刚才定义的子应用
+  location => location.pathname.startsWith('/vue')//用户切换到/vue的路径下，我们需要加载刚才定义的子应用
 )
 start()
 
